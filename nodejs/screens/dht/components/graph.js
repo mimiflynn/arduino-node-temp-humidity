36e9c350e0bf180b78658b8f4ffd18d1989c1f7c
Mimi Flynn <mimiflynn@gmail.com>
2016-08-21 09:47:34 -0400

    graph working

--- nodejs/screens/dht/components/graph.js
+++ nodejs/screens/dht/components/graph.js
@@ -1,101 +1,13 @@
 var React = require('react');
-var Chart = require('react-d3-core').Chart;
-var LineChart = require('react-d3-basic').LineChart;
-
 var d3 = require('d3');
+var moment = require('moment');
 
-var chartSeries = [
-  {
-    field: 'hif',
-    name: 'Dew Point',
-    color: '#ff0000'
-  }, {
-    field: 'fahrenheit',
-    name: 'Temp',
-    color: '#0000ff'
-  }
-];
+function InitChart (dht) {
 
-var x = function (d) {
-  console.log('d', d);
-  return d.fahrenheit;
-}
+  // Get time range from data
+  // assuming data is sorted in decending order
 
-var margins = {left: 100, right: 100, top: 50, bottom: 50};
-
-// ToDo - set up isomorphic rendering
-// refer to home.index
-// already rendering from server
-// need to re-render in client with data for chart
-var reactD3 = React.createClass({
-  propTypes: {
-    data: React.PropTypes.array
-  },
-  render: function () {
-    return (
-      <Chart
-        title="Title of Chart"
-        width={ 900 }
-        height={ 300 }
-        margins={ margins }
-        >
-        <LineChart
-          width={ 900 }
-          height={ 300 }
-          margins={ margins }
-          data={ this.props.data }
-          chartSeries={ chartSeries }
-          x={ x }
-        />
-      </Chart>
-    );
-  }
-});
-
-// module.exports = reactD3;
-
-
-function InitChart () {
-  var data = [{
-      "sale": "202",
-      "year": "2000"
-  }, {
-      "sale": "215",
-      "year": "2002"
-  }, {
-      "sale": "179",
-      "year": "2004"
-  }, {
-      "sale": "199",
-      "year": "2006"
-  }, {
-      "sale": "134",
-      "year": "2008"
-  }, {
-      "sale": "176",
-      "year": "2010"
-  }];
-
-
-  var data2 = [{
-      "sale": "152",
-      "year": "2000"
-  }, {
-      "sale": "189",
-      "year": "2002"
-  }, {
-      "sale": "179",
-      "year": "2004"
-  }, {
-      "sale": "199",
-      "year": "2006"
-  }, {
-      "sale": "134",
-      "year": "2008"
-  }, {
-      "sale": "176",
-      "year": "2010"
-  }];
+  var timeDomain = [new Date(dht[0].date), new Date(dht[119].date)];
 
   var vis = d3.select("#visualisation"),
       WIDTH = 1000,
@@ -107,9 +19,9 @@ function InitChart () {
           left: 50
       },
 
-      xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000, 2010]),
+      xScale = d3.scaleTime().range([MARGINS.left, WIDTH - MARGINS.right]).domain(timeDomain),
 
-      yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134, 215]),
+      yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([65, 100]),
 
       xAxis = d3.axisBottom()
       .scale(xScale),
@@ -129,20 +41,14 @@ function InitChart () {
 
   var lineGen = d3.line()
       .x(function (d) {
-          return xScale(d.year);
+          return xScale(new Date(d.date));
       })
       .y(function (d) {
-          return yScale(d.sale);
+          return yScale(d.fahrenheit);
       });
 
   vis.append('svg:path')
-      .attr('d', lineGen(data))
-      .attr('stroke', 'green')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none');
-
-  vis.append('svg:path')
-      .attr('d', lineGen(data2))
+      .attr('d', lineGen(dht))
       .attr('stroke', 'blue')
       .attr('stroke-width', 2)
       .attr('fill', 'none');
@@ -154,11 +60,11 @@ var chartD3 = React.createClass({
   },
   componentDidMount: function () {
     console.log('init chart');
-    InitChart();
+    InitChart(this.props.data);
   },
   render: function () {
     return (
-      <div id="mar" />
+      <h2>Temperature and humidity over time</h2>
     );
   }
 });
