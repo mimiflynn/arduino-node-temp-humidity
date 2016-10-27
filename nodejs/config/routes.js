990cdd92b52eb87e557778245686cd1917c59263
Mimi Flynn <mimiflynn@gmail.com>
2016-10-26 22:50:15 -0400

    more cors

--- nodejs/config/routes.js
+++ nodejs/config/routes.js
@@ -30,6 +30,7 @@ var commentAuth = [auth.requiresLogin, auth.comment.hasAuthorization];
 module.exports = function (app, passport) {
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
